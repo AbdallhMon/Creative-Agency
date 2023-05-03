ec967daf590b0096d5f3960a2b20bf409cbf3f73
AbdallhMon <131815737+AbdallhMon@users.noreply.github.com>
2023-05-03 16:57:12 +0300

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -55,10 +55,10 @@ colorsArr.forEach((ele) => {
 //changing background-img
 
 let imgArr = [
-  "url(imgs/01.jpg)",
-  "url(imgs/02.jpg)",
-  "url(imgs/03.jpg)",
-  "url(imgs/04.jpg)",
+  "url(../js/imgs/01.jpg)",
+  "url(../js/imgs/02.jpg)",
+  "url(../js/imgs/03.jpg)",
+  "url(../js/imgs/04.jpg)",
 ];
 
 //get from local
