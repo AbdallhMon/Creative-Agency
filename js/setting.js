c8f88c155fb952533b410f7e932d5adb8163a727
AbdallhMon <131815737+AbdallhMon@users.noreply.github.com>
2023-05-08 00:03:51 +0300

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -5,7 +5,7 @@ let colorsArr = Array.from(colors);
 
 let set = document.querySelector(".setting");
 
-let setButton = document.querySelector(".setting .setting-btn");
+let setButton = document.querySelector(".setting i");
 
 let landingPage = document.querySelector(".landing-page");
 
